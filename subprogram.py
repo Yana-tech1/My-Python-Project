f3a99ab9953a4fb8ddaefbb12bceaa0ab638f98a
Yana-tech1 <sokharath.y@api-school.com>
2025-06-11 08:29:06 +0700

    Create subprogram.py

---
+++ subprogram.py
@@ -0,0 +1,12 @@
+#subprogram
+#Procedure
+import random
+def fruits():
+    food = ["apple","banana","cherry","oranges"]
+    print(random.choice(food))
+fruits()
+
+#function
+def sum(x,y):
+    return (x + y)
+print(sum(1,7))
