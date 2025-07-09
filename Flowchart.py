4282feaf289aaf34356e3478dc5b9132301822e5
Yana-tech1 <sokharath.y@api-school.com>
2025-07-09 08:39:28 +0700

    Create Flowchart.py

---
+++ Flowchart.py
@@ -0,0 +1,12 @@
+answer = input("Are you hungry? Yes/No ->")
+if answer == "No":
+    print("Stop lying. Yes you are, big back...")
+    print("You should eat Instant Ramen :D")
+else:
+    print("Alright :D")
+    answer = input("Do you want to eat healthy? Yes/No ->")
+    if answer == "No":
+        print("You should eat Instant Ramen :D")
+    else:
+        print("No you don't...")
+        print("You should eat instant ramen :D")
